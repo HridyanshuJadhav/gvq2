525b592d3247651df0be3a900e8d0366dd89c67a
Hridyanshu Jadhav <104667988+HridyanshuJadhav@users.noreply.github.com>
2025-03-02 18:19:35 +0530

    Update contactus.php

--- Contact us page/contactus.php
+++ Contact us page/contactus.php
@@ -1,11 +1,19 @@
 <?php
-// Firebase Database ka URL
-$firebase_url = "https://gvq2-contactus-default-rtdb.firebaseio.com/";
+// Firebase Database URL -  ADD A PATH HERE!!!
+$firebase_url = "https://gvq2-contactus-default-rtdb.firebaseio.com/contacts.json"; //Important change
 
-// User Input
-$contactus_name = $_POST['contactus_name'];
-$contactus_email = $_POST['contactus_email'];
-$contactus_message = $_POST['contactus_message'];
+// User Input (Sanitize these inputs!)
+$contactus_name = filter_var($_POST['contactus_name'], FILTER_SANITIZE_STRING);
+$contactus_email = filter_var($_POST['contactus_email'], FILTER_SANITIZE_EMAIL);
+$contactus_message = filter_var($_POST['contactus_message'], FILTER_SANITIZE_STRING);
+
+//Avoid SQL injection-like vulnerabilities, since we aren't using SQL
+
+//Check for empty inputs:
+if(empty($contactus_name) || empty($contactus_email) || empty($contactus_message)){
+    echo "Please fill in all fields.";
+    exit;
+}
 
 // Data Array
 $data = [
@@ -14,23 +22,31 @@ $data = [
     "message" => $contactus_message
 ];
 
+// Generate a unique key (timestamp is a simple approach)
+$uniqueKey = time();
+
+// Update the Firebase URL to include the unique key
+$firebase_url = "https://gvq2-contactus-default-rtdb.firebaseio.com/contacts/{$uniqueKey}.json";
+
 // Convert data to JSON
 $json_data = json_encode($data);
 
 // cURL Setup
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $firebase_url);
-curl_setopt($ch, CURLOPT_POST, 1);
+$ch = curl_init($firebase_url); //Simplified curl initialization.
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); //Use POST method for creating new entries
 curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($json_data)));
+
 
 $response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-if ($response) {
+if ($httpCode == 200 || $httpCode == 201) { //Check for successful response codes
     echo "Data Submitted Successfully!";
 } else {
-    echo "Error!";
+    echo "Error submitting data. HTTP code: " . $httpCode;  //Provide more specific error message
 }
+
 ?>
