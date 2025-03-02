dd7f56182e15a078dcf0fbce116cc4e63e7395c1
Hridyanshu Jadhav <104667988+HridyanshuJadhav@users.noreply.github.com>
2025-03-02 18:39:27 +0530

    Update contactus.php

--- Contact us page/contactus.php
+++ Contact us page/contactus.php
@@ -1,56 +1,35 @@
 <?php
-// Firebase Database URL -  ADD A PATH HERE!!!
-$firebase_url = "https://gvq2-contactus-default-rtdb.firebaseio.com/contacts/test.json"; //Important change //remove /test from this text
 
-// User Input (Sanitize these inputs!)
-$contactus_name = filter_var($_POST['contactus_name'], FILTER_SANITIZE_STRING);
-$contactus_email = filter_var($_POST['contactus_email'], FILTER_SANITIZE_EMAIL);
-$contactus_message = filter_var($_POST['contactus_message'], FILTER_SANITIZE_STRING);
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gvq_major";
 
-//Avoid SQL injection-like vulnerabilities, since we aren't using SQL
+$con = mysqli_connect($server, $username, $password, $dbname);
 
-//Check for empty inputs:
-if(empty($contactus_name) || empty($contactus_email) || empty($contactus_message)){
-    echo "Please fill in all fields.";
-    exit;
+if(!$con){
+    echo "Not connected";
 }
 
-// Data Array
-$data = [
-    "name" => $contactus_name,
-    "email" => $contactus_email,
-    "message" => $contactus_message
-];
+else{
+    echo "Connected";
+}
 
-// Generate a unique key (timestamp is a simple approach)
-$uniqueKey = time();
+//start
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$contactus_name = $_POST['contactus_name'];
+$contactus_email = $_POST['contactus_email'];
+$contactus_message = $_POST['contactus_message'];
 
-// Update the Firebase URL to include the unique key
-$firebase_url = "https://gvq2-contactus-default-rtdb.firebaseio.com/contacts/{$uniqueKey}.json";
+$sql = "INSERT INTO test1 (contactus_name, contactus_email, contactus_message) 
+        VALUES ('$contactus_name', '$contactus_email', '$contactus_message')";
 
-// Convert data to JSON
-$json_data = json_encode($data);
+$result = mysqli_query($con, $sql);
 
-echo "<pre>";
-print_r($firebase_url);
-echo "</pre>"
-
-// cURL Setup
-$ch = curl_init($firebase_url); //Simplified curl initialization.
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); //Use POST method for creating new entries
-curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($json_data)));
-
-
-$response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-if ($httpCode == 200 || $httpCode == 201) { //Check for successful response codes
+if ($result) {
     echo "Data Submitted Successfully!";
 } else {
-    echo "Error submitting data. HTTP code: " . $httpCode;  //Provide more specific error message
+    echo "Error: " . mysqli_error($con);
 }
-
+}
 ?>
