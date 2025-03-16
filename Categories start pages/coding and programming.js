c814f2825a0a6779af9ca72889a0f2bd605aa859
deepikapatidar143 <deepatidar675@gmail.com>
2025-03-16 11:54:10 +0530

    Update coding and programming.js

--- Categories start pages/coding and programming.js
+++ Categories start pages/coding and programming.js
@@ -164,13 +164,13 @@ function coding_and_programming_level1_submit() {
     
   }
     coding_and_programming_result = coding_and_programming_l1_result;
-   if (funny_l1_result < 3) {
-    document.getElementById("funny_level1_nextlevel").disabled = true;
-    document.getElementById("funny_level1_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + funny_l1_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + funny_result + "</b> out of 21. You failed the level. You can't go to the next level. Please try again.";
+   if (coding_and_programming_l1_result < 3) {
+    document.getElementById("coding_and_programming_level1_nextlevel").disabled = true;
+    document.getElementById("coding_and_programming_level1_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_l1_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_result + "</b> out of 21. You failed the level. You can't go to the next level. Please try again.";
   }
-  if (funny_l1_result >= 3) {
-    document.getElementById("funny_level1_nextlevel").disabled = false;
-    document.getElementById("funny_level1_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + funny_l1_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + funny_result + "</b> out of 21. You passed the level. You can go to the next level.";
+  if (coding_and_programming_l1_result >= 3) {
+    document.getElementById("coding_and_programming_level1_nextlevel").disabled = false;
+    document.getElementById("coding_and_programming_level1_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_l1_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_result + "</b> out of 21. You passed the level. You can go to the next level.";
   }
 }
 function coding_and_programming_level1_reset() {
@@ -349,13 +349,13 @@ function coding_and_programming_level2_submit() {
     document.getElementById("q14_2_p").innerHTML = "&#x2713;";
   }
   coding_and_programming_result += coding_and_programming_l2_result;
- if (funny_l2_result < 3) {
-    document.getElementById("funny_level2_nextlevel").disabled = true;
-    document.getElementById("funny_level2_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + funny_l2_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + funny_result + "</b> out of 21. You failed the level. You can't go to the next level. Please try again.";
+ if (coding_and_programming_l2_result < 3) {
+    document.getElementById("coding_and_programming_level2_nextlevel").disabled = true;
+    document.getElementById("coding_and_programming_level2_result").innerHTML = "Your score of level 2 is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_l2_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_result + "</b> out of 21. You failed the level. You can't go to the next level. Please try again.";
   }
-  if (funny_l2_result >= 3) {
-    document.getElementById("funny_level2_nextlevel").disabled = false;
-    document.getElementById("funny_level2_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + funny_l2_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + funny_result + "</b> out of 21. You passed the level. You can go to the next level.";
+  if (coding_and_programming_l2_result >= 3) {
+    document.getElementById("coding_and_programming_level2_nextlevel").disabled = false;
+    document.getElementById("coding_and_programming_level2_result").innerHTML = "Your score of level 2 is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_l2_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_result + "</b> out of 21. You passed the level. You can go to the next level.";
   }
   
 }
@@ -538,11 +538,11 @@ function coding_and_programming_level3_submit() {
   }
   coding_and_programming_result += coding_and_programming_l3_result;
   if (coding_and_programming_l3_result < 3) {
-    document.getElementById("coding_and_programming_level3_result").innerHTML = "Your score is "+ coding_and_programming_l3_result + "out of 7. You failed the level. You can't go to the next level. Please try again.";
-   }
-   if (coding_and_programming_l3_result >= 3) {
-   document.getElementById("coding_and_programming_level3_result").innerHTML = "Your score is "+ coding_and_programming_l3_result + "out of 7. You passed the level. You can go to the next level.";
-   }
+    document.getElementById("coding_and_programming_level3_result").innerHTML = "Your score of level 3 is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_l3_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_result + "</b> out of 21. You failed the level. You can't go to the next level. Please try again.";
+  }
+  if (coding_and_programming_l3_result >= 3) {
+    document.getElementById("coding_and_programming_level3_result").innerHTML = "Your score of level 3 is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_l3_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + coding_and_programming_result + "</b> out of 21. You passed the level. You can go to the next level.";
+  }
   
 }
 function coding_and_programming_level3_reset() {
