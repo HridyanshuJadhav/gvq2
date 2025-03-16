2fc6c8d2507b9f5094f3a96e49c36057f3f776fe
deepikapatidar143 <deepatidar675@gmail.com>
2025-03-16 11:49:17 +0530

    Update coding and programming.js

--- Categories start pages/coding and programming.js
+++ Categories start pages/coding and programming.js
@@ -164,15 +164,14 @@ function coding_and_programming_level1_submit() {
     
   }
     coding_and_programming_result = coding_and_programming_l1_result;
-  if (coding_and_programming_l1_result < 3) {
-    document.getElementById("coding_and_programming_level1_nextlevel").disabled = true;
-    document.getElementById("coding_and_programming_level1_result").innerHTML = "Your score is "+ coding_and_programming_l1_result + "out of 7. You failed the level. You can't go to the next level. Please try again.";
-   }
-   if (coding_and_programming_l1_result >= 3) {
-      document.getElementById("coding_and_programming_level1_nextlevel").disabled = false;
-     document.getElementById("coding_and_programming_level1_result").innerHTML = "Your score is "+ coding_and_programming_l1_result + "out of 7. You passed the level. You can go to the next level.";
-   
- }
+   if (funny_l1_result < 3) {
+    document.getElementById("funny_level1_nextlevel").disabled = true;
+    document.getElementById("funny_level1_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + funny_l1_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + funny_result + "</b> out of 21. You failed the level. You can't go to the next level. Please try again.";
+  }
+  if (funny_l1_result >= 3) {
+    document.getElementById("funny_level1_nextlevel").disabled = false;
+    document.getElementById("funny_level1_result").innerHTML = "Your score of level 1 is <b style='text-shadow:0 0 20px black;'>" + funny_l1_result + "</b> out of 7 and your total score is <b style='text-shadow:0 0 20px black;'>" + funny_result + "</b> out of 21. You passed the level. You can go to the next level.";
+  }
 }
 function coding_and_programming_level1_reset() {
   for (let i = 1; i <= 20; i++) {
