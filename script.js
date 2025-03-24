37c71adcff3cfdd5b3349b9cadcd8cf536697c40
Hridyanshu Jadhav <104667988+HridyanshuJadhav@users.noreply.github.com>
2025-03-25 00:09:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 setTimeout(() => {
     document.getElementById("loader-maindiv").style.display = "none";
 }, 3000);
+
+window.onload = function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
