125c4e24ac53bb04eea0dc1985ed75eb90706421
Hridyanshu Jadhav <104667988+HridyanshuJadhav@users.noreply.github.com>
2025-03-25 15:48:51 +0530

    Create signup.php

---
+++ login signup page/signup.php
@@ -0,0 +1,43 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gvq_major";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
+
+if(!$con){
+    echo "Not connected";
+}
+
+else{
+    echo "Connected";
+}
+
+//start
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$signup_name = $_POST['signup_name'];
+$signup_email = $_POST['signup_email'];
+$signup_password = $_POST['signup_password'];
+$signup_confirm = $_POST['signup_confirm'];
+
+if ($signup_password !== $signup_confirm) {
+    echo "<script>alert('Passwords do not match!'); window.location.href='/gvq2/login signup page/signup.html';</script>";
+    exit();
+}
+
+$hashed_password = password_hash($signup_password, PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO userdata (signup_name, signup_email, signup_password) 
+        VALUES ('$signup_name', '$signup_email', '$hashed_password')";
+
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+    echo "<script>alert('Data Submitted Successfully!'); widnoe.location.href='/gvq2/login signup page/login.html'</script>";
+} else {
+    echo "Error: " . mysqli_error($con);
+}
+}
+?>
