2e2d8ebe22819ed55977e95f35e4d1e867b4d121
Hridyanshu Jadhav <104667988+HridyanshuJadhav@users.noreply.github.com>
2025-03-25 16:14:32 +0530

    Create feedback.php

---
+++ Feedback page/feedback.php
@@ -0,0 +1,27 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gvq_major";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
+
+//start
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$feedback_name = $_POST['feedback_name'];
+$feedback_email = $_POST['feedback_email'];
+$feedback_textarea = $_POST['feedback_textarea'];
+
+$sql = "INSERT INTO feedback (feedback_name, feedback_email, feedback_textarea) 
+        VALUES ('$feedback_name', '$feedback_email', '$feedback_textarea')";
+
+$result = mysqli_query($con, $sql);
+
+if ($result) {
+    echo "Data Submitted Successfully!";
+} else {
+    echo "Error: " . mysqli_error($con);
+}
+}
+?>
