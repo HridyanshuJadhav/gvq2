50bdc3db6d980b75cea1da062ecdb25ddd9d3c5b
Hridyanshu Jadhav <104667988+HridyanshuJadhav@users.noreply.github.com>
2025-03-27 17:25:40 +0530

    Update contactus.php

--- Contact us page/contactus.php
+++ Contact us page/contactus.php
@@ -7,27 +7,19 @@ $dbname = "gvq_major";
 
 $con = mysqli_connect($server, $username, $password, $dbname);
 
-if(!$con){
-    echo "Not connected";
-}
-
-else{
-    echo "Connected";
-}
-
 //start
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $contactus_name = $_POST['contactus_name'];
 $contactus_email = $_POST['contactus_email'];
 $contactus_message = $_POST['contactus_message'];
 
-$sql = "INSERT INTO test1 (contactus_name, contactus_email, contactus_message) 
+$sql = "INSERT INTO contactus (contactus_name, contactus_email, contactus_message) 
         VALUES ('$contactus_name', '$contactus_email', '$contactus_message')";
 
 $result = mysqli_query($con, $sql);
 
 if ($result) {
-    echo "Data Submitted Successfully!";
+    echo "<script>alert('Data Submitted Successfully!'); window.location.href='/gvq2/Contact us page/contactus.html';</script>";
 } else {
     echo "Error: " . mysqli_error($con);
 }
